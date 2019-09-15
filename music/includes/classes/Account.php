a7d0245675bb331c10d32fae2572c84dd7afee6a
Tom Zheng <1219.zheng@gmail.com>
2019-09-15 15:07:34 -0700

    up login in sign in sign up register UI

---
+++ music/includes/classes/Account.php
@@ -0,0 +1,155 @@
+<?php
+	class Account {
+
+		private $con;
+		private $errorList;
+
+		public function __construct($con) {
+			$this->con = $con;
+			$this->errorList = array();
+		}
+
+        public function login($un, $pw)
+        {
+
+            $pw = sha1($pw);  //The sha1() function uses the US Secure Hash Algorithm 1.
+            $query=mysqli_query($this->con,"SELECT * FROM users WHERE username='$un' AND password='$pw'");
+            if (mysqli_num_rows($query) == 1) {
+                return true;
+            } else {
+                array_push($this->errorList, Constants::$loginFailed);
+                return false;
+            }
+        }
+
+		public function register($un, $fn, $ln, $em, $pw, $pw2) {
+			$this->validateUsername($un);
+			$this->validateFirstName($fn);
+			$this->validateLastName($ln);
+			$this->validateEmails($em);
+			$this->validatePasswords($pw, $pw2);
+
+			if(empty($this->errorList) == true) {//empty — Determine whether a variable is empty
+				//Insert into db
+				return $this->insertUserDetails($un, $fn, $ln, $em, $pw);
+			}
+			else {
+				return false;
+			}
+		}
+
+		public function getError($error) {
+			if(!in_array($error, $this->errorList)) {//Checks if a value exists in an array
+				$error = "";
+			}
+			return "<span class='errorMessage'>$error</span>";
+		}
+
+		/**
+		 * For security function
+		 */
+		public function encryptPassword($password){
+			$salt1 = "*&g!";
+			$salt2 = "hb%$";
+			$token = hash('ripemd128', "$salt1$password$salt2");
+			return $token;
+		}
+
+		//sanitazing from MySQL
+		public function mysql_fix_string($connection, $string)
+		{
+			if(get_magic_quotes_gpc())
+				$string = stripslashes($string);
+			return $connection->real_escape_string($string);
+		}
+
+		//sanitazing from HTML
+		public function mysql_entities_fix_string($connection, $string)
+		{
+			return htmlentities(mysql_fix_string($connection, $string));
+		}
+
+		/**
+		 * the below is the private functions to implement register and login event
+		 */
+		private function insertUserDetails($un, $fn, $ln, $em, $pw) {
+//			$encryptedPw = md5($pw);            //Password ->  8b1a9953c4611296a827abf8c47804d7
+			//or we w3c school find the example, md5 is a  The MD5 Message-Digest Algorithm
+            $encryptedPw = sha1($pw); //The sha1() function uses the US Secure Hash Algorithm 1.
+			$profilePic = "component/images/profile/head.png";
+			$date = date("Y-m-d");
+
+			$result = mysqli_query($this->con, "INSERT INTO users VALUES (NULL, '$un', '$fn', '$ln', '$em', '$encryptedPw', '$date', '$profilePic')");
+
+			return $result;
+		}
+
+		private function validateUsername($un) {
+
+			if(strlen($un) > 25 || strlen($un) < 3) {
+				array_push($this->errorList, Constants::$usernameCharacters);
+				return;
+			}
+
+			$checkUsernameQuery = mysqli_query($this->con,"SELECT username FROM users WHERE username='$un'");
+            if (mysqli_num_rows($checkUsernameQuery) != 0) {//Return the number of rows in a result set
+                array_push($this->errorList, Constants::$usernameTaken);
+                return;
+            }
+		}
+
+		private function validateFirstName($fn) {
+			if(strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorList, Constants::$firstNameCharacters);
+				return;
+			}
+		}
+
+		private function validateLastName($ln) {
+			if(strlen($ln) > 25 || strlen($ln) < 2) {
+				array_push($this->errorList, Constants::$lastNameCharacters);
+				return;
+			}
+		}
+
+		private function validateEmails($em) {
+			// if($em != $em2) {
+			// 	array_push($this->errorList, Constants::$emailsDoNotMatch);
+			// 	return;
+			// }
+
+		    /* The filter_var() function filters a variable with the specif ied filter. 
+			This function is used to both validate and sanitize the data. */
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorList, Constants::$emailInvalid);
+				return;
+			}
+
+			//TODO: Check that username hasn't already been used
+            $checkEmailQuery = mysqli_query($this->con,"SELECT email FROM users WHERE email='$em'");
+            if (mysqli_num_rows($checkEmailQuery) != 0) {//Return the number of rows in a result set
+                array_push($this->errorList, Constants::$emailTaken);
+                return;
+            }
+		}
+
+		private function validatePasswords($pw, $pw2) {
+			
+			if($pw != $pw2) {
+				array_push($this->errorList, Constants::$passwordsDoNoMatch);
+				return;
+			}
+
+			if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+				array_push($this->errorList, Constants::$passwordIsIllegal);
+				return;
+			}
+
+			if(strlen($pw) > 30 || strlen($pw) < 5) {
+				array_push($this->errorList, Constants::$passwordCharacters);
+				return;
+			}
+
+		}
+	}
+?>
---
+++ music/register.php
@@ -0,0 +1,119 @@
+<?php
+	include("includes/config.php");
+	include("includes/classes/Account.php");
+	include("includes/classes/Constants.php");
+
+	$account = new Account($con);
+
+	include("includes/handlers/register-handler.php");
+    include("includes/handlers/login-handler.php");
+    include("includes/handlers/switchBtn.php");
+
+	function getInputValue($name) {
+		if(isset($_POST[$name])) {
+			echo $_POST[$name];
+		}
+	}
+?>
+
+<html lang="">
+<head>
+	<title>Welcome to Streaming Music!</title>
+    <link rel="stylesheet" type="text/css" href="component/css/register.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="component/js/register.js"></script>
+</head>
+<body>
+<?php
+switchButton('registerButton');
+?>
+    <div id="background">
+        <div id="loginContainer">
+            <div id="inputContainer">
+
+                <form id="loginForm" action="register.php" method="POST">
+                    <h2>Login to your account</h2>
+                    <p>
+                        <?php echo $account->getError(Constants::$loginFailed); ?>
+                        <label for="loginUsername">Username</label>
+                        <input id="loginUsername" name="loginUsername" type="text" placeholder="username" 
+                        value="<?php getInputValue('loginUsername') ?>" required>
+                    </p>
+                    <p>
+                        <label for="loginPassword">Password</label>
+                        <input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+                    </p>
+
+                    <button type="submit" name="loginButton">LOG IN</button>
+
+                    <div class="hasAccountText">
+                        <span id="hideLogin">If you don't have a account, please sign up</span>
+                    </div>
+
+                </form>
+
+                <form id="registerForm" action="register.php" method="POST">
+                    <h2>Create your free account</h2>
+                    <p>
+                        <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                        <?php echo $account->getError(Constants::$usernameTaken); ?>
+                        <label for="username">Username</label>
+                        <input id="username" name="username" type="text" placeholder="username" value="<?php getInputValue('username') ?>" required>
+                    </p>
+
+                    <p>
+                        <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                        <label for="firstName">First name</label>
+                        <input id="firstName" name="firstName" type="text" placeholder="first name" value="<?php getInputValue('firstName') ?>" required>
+                    </p>
+
+                    <p>
+                        <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                        <label for="lastName">Last name</label>
+                        <input id="lastName" name="lastName" type="text" placeholder="last name" value="<?php getInputValue('lastName') ?>" required>
+                    </p>
+
+                    <p>
+                        <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                        <?php echo $account->getError(Constants::$emailInvalid); ?>
+                        <?php echo $account->getError(Constants::$emailTaken); ?>
+                        <label for="email">Email</label>
+                        <input id="email" name="email" type="email" placeholder="name@gmail.com" value="<?php getInputValue('email') ?>" required>
+                    </p>
+
+                    <p>
+                        <?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+                        <?php echo $account->getError(Constants::$passwordIsIllegal); ?>
+                        <?php echo $account->getError(Constants::$passwordCharacters); ?>
+                        <label for="password">Password</label>
+                        <input id="password" name="password" type="password" placeholder="Your password" required>
+                    </p>
+
+                    <p>
+                        <label for="password2">Confirm password</label>
+                        <input id="password2" name="password2" type="password" placeholder="Your password" required>
+                    </p>
+
+                    <button type="submit" name="registerButton">SIGN UP</button>
+
+                    <div class="hasAccountText">
+                        <span id="hideRegister">Pleae log in</span>
+                    </div>
+                </form>
+
+            </div>
+
+            <div id="loginText">
+                <h1>CS157A Team-20 Streming Music </h1>
+                <h2>Listen our songs for free</h2>
+                <ul>
+                    <li>Discover music you'll fall in love with</li>
+                    <li>Create your own playlists</li>
+                    <li>Follow artists to keep up to date</li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
