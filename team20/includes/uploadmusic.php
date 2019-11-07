7f4056ec12d72504f6fe3216db71c4fc786e4486
khoaLib <phamkhoa996@gmail.com>
2019-11-06 17:39:45 -0800

    create a way to upload music to server and to MySQL

---
+++ team20/includes/uploadmusic.php
@@ -0,0 +1,50 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "team20");
+if($con){
+    echo "Good Connection";
+}
+if(mysqli_connect_errno()) {//Return an error description from the last connection error
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+//
+if(isset($_POST['submit'])){
+    $name= $_FILES['file']['name'];
+    $temp=$_FILES['file']['tmp_name'];
+
+    move_uploaded_file($temp,"musics/".$name);
+    $url = "http://localhost/team20/includes/musics/$name";
+    //mysql_query("INSERT INTO 'music' VALUE ('','$name','$url')");
+    $insertMusic = mysqli_query($con, "INSERT INTO musicS VALUES ('','$name','$url')");
+    //echo "Good we get that music <br>";
+    //mysql_query();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset = "utf-8">
+    <title>Upload music to web</title>
+    <style>
+        /*background-color: lightblue;*/
+        p {
+            color: springgreen;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form action="uploadmusic.php" method="POST" ENCTYPE="multipart/form-data">
+    <input type="file" name="file"/>
+    <input type="submit" name="submit" value="upload"/>
+        <?php
+            if(isset($_POST['submit'])){
+                echo "<br/>".$name." has been uploaded";
+            }
+        ?>
+    </form>
+
+
+</body>
+</html>
