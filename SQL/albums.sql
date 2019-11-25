d7c7671d450cde154f43407f197f106e8aa42220
khoaLib <phamkhoa996@gmail.com>
2019-11-25 13:50:01 -0800

    Upload some change to table contents

--- SQL/albums.sql
+++ SQL/albums.sql
@@ -12,18 +12,18 @@ CREATE TABLE IF NOT EXISTS `albums` (
 -- Insert data into albums
 
 INSERT INTO `albums` (`id`, `title`, `artist`, `genre`, `artworkPath`) VALUES
-(1, 'Macbook Pro', 2, 4, 'assets/images/artwork/mbp.png'),
-(2, 'De Anza College', 5, 10, 'assets/images/artwork/react.png'),
-(3, 'Tom Zheng', 3, 1, 'assets/images/artwork/alb.png'),
-(4, 'IOS', 2, 9, 'assets/images/artwork/ios.png'),
-(5, 'MySQL', 1, 3, 'assets/images/artwork/mysql.png'),
-(6, 'JDBC', 3, 6, 'assets/images/artwork/java.png'),
-(7, 'React.JS', 4, 7, 'assets/images/artwork/react.png'),
-(8, 'Tesla', 5, 9, 'assets/images/artwork/tesla.png'),
-(9, 'VUE.JS', 1, 8, 'assets/images/artwork/vuejs.png'),
-(10, 'Angular.JS', 1, 3, 'assets/images/artwork/angular.png'),
-(11, 'android', 4, 5, 'assets/images/artwork/android.png'),
-(12, 'Tomorrow', 4, 3, 'assets/images/artwork/album.png'),
-(13, 'VScode', 1, 9, 'assets/images/artwork/vs.png'),
-(14, 'Swift', 2, 8, 'assets/images/artwork/swift.png'),
-(15, 'Python', 3, 6, 'assets/images/artwork/python.png');
+(1, 'Macbook Pro', 5, 4, 'resource/images/artwork/mbp.png'),
+(2, 'De Anza College', 4, 10, 'resource/images/artwork/react.png'),
+(3, 'Tom Zheng', 3, 1, 'resource/images/artwork/alb.png'),
+(4, 'IOS', 2, 9, 'resource/images/artwork/ios.png'),
+(5, 'MySQL', 1, 3, 'resource/images/artwork/mysql.png'),
+(6, 'JDBC', 6, 6, 'resource/images/artwork/java.png'),
+(7, 'React.JS', 7, 7, 'resource/images/artwork/react.png'),
+(8, 'Tesla', 8, 11, 'resource/images/artwork/tesla.png'),
+(9, 'VUE.JS', 9, 8, 'resource/images/artwork/vuejs.png'),
+(10, 'Angular.JS', 10, 12, 'resource/images/artwork/angular.png'),
+(11, 'android', 11, 13, 'resource/images/artwork/android.png'),
+(12, 'Tommorrow', 12, 14, 'resource/images/artwork/album.png'),
+(13, 'VScode', 13, 15, 'resource/images/artwork/vs.png'),
+(14, 'Swift', 14, 2, 'resource/images/artwork/swift.png'),
+(15, 'Python', 15, 5, 'resource/images/artwork/python.png');
--- SQL/musics.sql
+++ SQL/musics.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS musics (
  id int(11) NOT NULL AUTO_INCREMENT,
- name varchar(50) NOT NULL,
+ name varchar(150) NOT NULL,
  url VARCHAR (200) NOT NULL,
  PRIMARY KEY(id)
 ) ENGINE=InnoDB ;
