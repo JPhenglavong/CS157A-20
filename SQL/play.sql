d1a1d6aa20cbaaf7b216f2f1a35e2d7e1a129c8d
khoaLib <phamkhoa996@gmail.com>
2019-11-25 16:53:56 -0800

    add play relationship between user and song 'many-many'

---
+++ SQL/play.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS play (
+    userID int(11) NOT NULL,
+     songID int(11) NOT NULL
+) ENGINE=InnoDB;
+
+select * from play;
+
+insert into play
+values
+(3,10),
+(1,2),
+(1,8),
+(1,3),
+(2,4),
+(2,6),
+(2,9),
+(8,11),
+(5,13),
+(6,15)
+
+;
