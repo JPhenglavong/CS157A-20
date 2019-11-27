cd14898a29a503f4f4a15d8dbfc84276ee7af151
JPhenglavong <JPhenglavong@yahoo.com>
2019-11-26 23:14:37 -0800

    update includes

---
+++ includes/manipulateDatabase.php
@@ -0,0 +1,17 @@
+<?php
+
+function getDataFromAlbums($query){
+    while($row = mysqli_fetch_array($query)){
+        echo "<div class='gridViewItem'>
+                <a href='albumPage.php?id=" . $row['id'] . "'> 
+                    <img src='" . $row['artworkPath'] . "'>
+
+                    <div class='gridViewInfo'>"
+                        . $row['title'] .
+                    "</div>
+                </a>
+            </div>";
+    }
+}
+
+?>
