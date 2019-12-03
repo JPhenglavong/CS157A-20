7c258b97d4411befe12775e260788a574eaeda9d
Khoa Pham <45501926+khoaLib@users.noreply.github.com>
2019-12-02 21:07:23 -0800

    Update artist.sql

--- SQL/artist.sql
+++ SQL/artist.sql
@@ -1,15 +1,26 @@
--- Table structure for table artists
-
 CREATE TABLE IF NOT EXISTS artists (
     id int(11) NOT NULL,
      name varchar(50) NOT NULL
 ) ENGINE=InnoDB;
-
--- Data for table artists
-
+--
+-- Dumping data for table artists
+--
 INSERT INTO artists (id, name) VALUES
-(1, 'J. Cole'),
-(2, 'Kendrick Lamar'),
-(3, 'Roddy Ricch'),
-(4, 'Ezale'),
-(5, 'Michael Wu');
+(1, 'harry potter'),
+(2, 'John'),
+(3, 'Jack'),
+(4, 'Rose'),
+(5, 'Mike Wu'),
+(6, 'Tom Zheng'),
+(7, 'Jack Bauer 24Hr'),
+(8, 'Khaleesi'),
+(9, 'Breaking Bad'),
+(10, 'Michael Scofield Prison Break'),
+(11, 'Mickey Mouse'),
+(12, 'Tim Cook'),
+(13, 'J. Cole'),
+(14, 'Kendrick Lamar'),
+(15, 'Roddy Ricch'),
+(16, 'Ezale'),
+(17, 'Michael Wu');
+
