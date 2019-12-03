16f94f769a10c0a5ed93b39a4f45363f5404de5c
khoaLib <phamkhoa996@gmail.com>
2019-12-02 21:43:38 -0800

    update playlist sql code

---
+++ SQL/Playlist.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `Playlist` (
+    `id` int(11) NOT NULL,
+    `name` varchar(250) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+INSERT INTO `Playlist` (`id`, `name`) VALUES
+(1, 'Ends of the year'),
+(2, 'animes series'),
+(3, 'Hip-hop'),
+(4, 'Pop music'),
+(5, 'gyms songs'),
+(6, 'aniversary'),
+(7, 'Love lists of mine'),
+(8, 'my friend bad list'),
+(9, 'fairy tale'),
+(10, 'love songs'),
+(11, 'happy songs'),
+(12, 'workout'),
+(13, 'music of the morning'),
+(14, 'Night is not bad'),
+(15, 'new branch will have to listen to');
